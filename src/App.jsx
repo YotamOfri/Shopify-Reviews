13beb906a2d0de0d54b17b245a97abd7ddfefb62
ProPotam <yotamofri.b@gmail.com>
2024-01-30 20:10:49 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Form from "./components/form";
+import Form from "./components/Form";
 export default function App() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
